6b06cc255847605e43acf3f3bbf3d83b898611d4
hayat-lf <gtrapti21@gmail.com>
2023-12-11 01:29:44 +0530

    Inception

---
+++ Namaste_React/Food_Ordering_App/App.js
@@ -0,0 +1,11 @@
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "hello form react"
+// );
+const heading = React.createElement("h1", { id: "parent" }, [
+  React.createElement("h1", { id: "child" }, "first child"),
+  React.createElement("h1", { id: "child" }, "Second child"),
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
